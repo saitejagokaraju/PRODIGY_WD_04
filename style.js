11001975001f5a8ff0da82dbe9b38b1c6033409c
saitejagokaraju <gokarajusaiteja26@gmail.com>
2023-10-28 18:57:08 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,7 @@
+const blurHeader = () => {
+    const header = document.getElementById("header");
+    this.scrollY >= 50
+        ? header.classList.add("blur-header")
+        : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
